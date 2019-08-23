a61db49e76d12224b3315ecbe861873048592ec4
Tyler ONeill <tyleroneill21@gmail.com>
2019-08-23 00:33:30 -0700

    Second go at it.
    started to keep track of more things...

--- eloquentjavascript/deepcheck.js
+++ eloquentjavascript/deepcheck.js
@@ -1,23 +1,40 @@
+let base = 0;
+let keeptrack = [];
 //let a = [1, 2, 3, 4];
 //let b = [1, 2, 3, 4];
-let a = {a: 1, b: 2, c: 3, food: 'apple'};
-let b = {a: 1, b: 2, c: 3, food: 'apple'};
+let a = {a: 1, b: 2, c: 3, d: {a: 1, g: 6, hello: 'world'}, food: 'apple'};
+let b = {a: 1, b: 2, c: 3, d: {a: 1, g: 6, hello: 'world'}, food: 'apple'};
 deepequal = (a, b) => {
     if(typeof a === 'object' && typeof b === 'object') {
         console.log('loop through objects')
         let akeys = Object.keys(a);
         let bkeys = Object.keys(b);
+        //bkeys may not be needed because akeys should work, if not, false
 
         if(a.length == b.length){           
-            for(let i = 0; i < akeys.length; i++){                
-                if(typeof a[akeys[i]] == 'object' && typeof b[bkeys[i]] == 'object'){
-                    deepqual(a[akeys[i]], b[bkeys[i]]);
+            for(let i = 0; i < akeys.length; i++){  
+                if(keeptrack.length > 0){
+                    i = keeptrack.pop() + 1;
+                }              
+                if(typeof a[akeys[i]] == 'object' && typeof b[akeys[i]] == 'object'){
+                    keeptrack.push(i);
+                    base += 1;
+                    deepequal(a[akeys[i]], b[bkeys[i]]);
                     //only really recursive if it finds an object within an object.
                 }
-                if(a[akeys[i]] === b[bkeys[i]]){
+                //by using akeys on both sides, we simultaniously check that the property names match as well.
+                if(a[akeys[i]] === b[akeys[i]]){
                     console.log(a[akeys[i]] + ' = ' + b[bkeys[i]]);
                     if(i == akeys.length - 1){
-                        return console.log('true');
+                        if(base == 0){
+                            return console.log('true');
+                        } else {                       
+                            
+                            console.log('object passed: level + ' + base);
+                            base -= 1;
+                            console.log(i);
+                        }
+                        
                     }
                 } else {
                     return console.log('false');
